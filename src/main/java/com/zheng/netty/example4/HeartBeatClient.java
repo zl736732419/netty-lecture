0b5630eca8b7929a2a93f2cf01fe8c7a56534ab3
z736732419 <736732419@qq.com>
2019-05-01 10:42:58 +0800

    优化心跳机制，实现空闲指定次数后断开连接

--- src/main/java/com/zheng/netty/example4/HeartBeatClient.java
+++ src/main/java/com/zheng/netty/example4/HeartBeatClient.java
@@ -11,7 +11,6 @@ import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.codec.string.StringEncoder;
-import io.netty.handler.timeout.IdleStateHandler;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -32,8 +31,8 @@ public class HeartBeatClient {
                         @Override
                         protected void initChannel(SocketChannel ch) throws Exception {
                             ChannelPipeline pipeline = ch.pipeline();
-                            pipeline.addLast(new IdleStateHandler(5, 3, 10))
-                                    .addLast(new StringDecoder())
+//                            pipeline.addLast(new IdleStateHandler(5, 3, 10))
+                              pipeline.addLast(new StringDecoder())
                                     .addLast(new StringEncoder())
                                     .addLast(new HeartBeatClientHandler());
                         }
--- src/main/java/com/zheng/netty/example4/HeartBeatServerHandler.java
+++ src/main/java/com/zheng/netty/example4/HeartBeatServerHandler.java
@@ -7,6 +7,7 @@ import io.netty.handler.timeout.IdleStateEvent;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @Author zhenglian
@@ -14,9 +15,10 @@ import java.util.Date;
  */
 public class HeartBeatServerHandler extends SimpleChannelInboundHandler<String> {
     private int idleCount = 3;
-    private int currentCount = 0;
+    private AtomicInteger currentCount = new AtomicInteger(0);
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
+        currentCount.set(0);
         System.out.println("收到客户端消息：" + msg);
         SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         ctx.channel().writeAndFlush(sf.format(new Date()));
@@ -42,20 +44,16 @@ public class HeartBeatServerHandler extends SimpleChannelInboundHandler<String> 
                 case READER_IDLE:
                     eventType = "读空闲";
                     break;
-                case WRITER_IDLE:
-                    eventType = "写空闲";
-                    break;
                 case ALL_IDLE:
                     eventType = "读写空闲";
                     break;
             }
             System.out.println(eventType);
-            currentCount++;
             // 发生心跳检测异常，关闭连接
-            if (currentCount > idleCount) {
+            if (currentCount.incrementAndGet() > idleCount) {
                 ctx.channel().close();
             } else {
-                System.out.println("客户端连接["+ctx.channel().remoteAddress()+"]空闲：" + currentCount + "次");
+                System.out.println("客户端连接["+ctx.channel().remoteAddress()+"]空闲：" + currentCount.get() + "次");
             }
         } else {
             super.userEventTriggered(ctx, evt);
