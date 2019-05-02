91125a5b849c231436a8e52fa4c0f5e9833afc06
z736732419 <736732419@qq.com>
2019-05-02 22:51:55 +0800

    优化 thrift client, server

--- src/main/java/com/zheng/thrift/ThriftClient.java
+++ src/main/java/com/zheng/thrift/ThriftClient.java
@@ -3,7 +3,9 @@ package com.zheng.thrift;
 import com.zheng.thrift.generated.Person;
 import com.zheng.thrift.generated.PersonService;
 import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.protocol.TCompactProtocol;
 import org.apache.thrift.protocol.TProtocol;
+import org.apache.thrift.transport.TFramedTransport;
 import org.apache.thrift.transport.TSocket;
 import org.apache.thrift.transport.TTransport;
 
@@ -13,23 +15,46 @@ import org.apache.thrift.transport.TTransport;
  */
 public class ThriftClient {
     public static void main(String[] args) throws Exception {
+//        client1();
+        client2();
+    }
+
+    private static void client2() {
+        TTransport transport = new TFramedTransport(new TSocket("localhost", 8899));
+        TProtocol protocol = new TCompactProtocol(transport);
+        PersonService.Client client = new PersonService.Client(protocol);
+        try {
+            transport.open();
+            execute(client);
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage(), e);
+        } finally {
+            transport.close();
+        }
+    }
+
+    private static void client1() throws Exception {
         TTransport transport = new TSocket("localhost", 8899);
         transport.open();
         TProtocol protocol = new TBinaryProtocol(transport);
         PersonService.Client client = new PersonService.Client(protocol);
+        execute(client);
+        // 执行完成调用后关闭连接
+        transport.close();
+    }
+
+    private static void execute(PersonService.Client client) throws Exception {
         // 执行rpc
         Person person = client.getPersonByUsername("张三");
         System.out.println("username: " + person.getUsername());
         System.out.println("age: " + person.getAge());
         System.out.println("married: " + person.isMarried());
-        
+
         System.out.println("----------------");
         Person person1 = new Person();
         person1.setUsername("李四");
         person1.setAge(25);
         person1.setMarried(false);
         client.savePerson(person1);
-        // 执行完成调用后关闭连接
-        transport.close();
     }
 }
--- src/main/java/com/zheng/thrift/ThriftServer.java
+++ src/main/java/com/zheng/thrift/ThriftServer.java
@@ -1,8 +1,13 @@
 package com.zheng.thrift;
 
 import com.zheng.thrift.generated.PersonService;
+import org.apache.thrift.TProcessorFactory;
+import org.apache.thrift.protocol.TCompactProtocol;
+import org.apache.thrift.server.THsHaServer;
 import org.apache.thrift.server.TServer;
 import org.apache.thrift.server.TSimpleServer;
+import org.apache.thrift.transport.TFramedTransport;
+import org.apache.thrift.transport.TNonblockingServerSocket;
 import org.apache.thrift.transport.TServerSocket;
 import org.apache.thrift.transport.TServerTransport;
 
@@ -12,11 +17,32 @@ import org.apache.thrift.transport.TServerTransport;
  */
 public class ThriftServer {
     public static void main(String[] args) throws Exception {
+//        server1();
+        server2();
+    }
+
+    private static void server2() throws Exception {
+        TNonblockingServerSocket socket = new TNonblockingServerSocket(8899);
+        THsHaServer.Args args = new THsHaServer.Args(socket)
+                .minWorkerThreads(2)
+                .maxWorkerThreads(4);
+        PersonService.Processor<PersonServiceImpl> processor = new PersonService.Processor<>(new PersonServiceImpl());
+        
+        args.protocolFactory(new TCompactProtocol.Factory());
+        args.transportFactory(new TFramedTransport.Factory());
+        args.processorFactory(new TProcessorFactory(processor));
+        
+        TServer server = new THsHaServer(args);
+        System.out.println("server listen on 8899.");
+        server.serve();
+    }
+
+    private static void server1() throws Exception {
         TServerTransport transport = new TServerSocket(8899);
 
         PersonServiceImpl handler = new PersonServiceImpl();
         PersonService.Processor<PersonServiceImpl> processor = new PersonService.Processor<>(handler);
-        
+
         TServer server = new TSimpleServer(new TServer.Args(transport).processor(processor));
         System.out.println("server listen on 8899.");
         server.serve();
